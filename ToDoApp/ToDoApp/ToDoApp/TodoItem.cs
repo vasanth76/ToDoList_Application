ef31741ecb2edc8d717e1ea0f95fd5d1fc6c2f66
Vasanth <68063138+vasanth76@users.noreply.github.com>
2022-04-02 09:44:52 +0530

    FirstCommit

---
+++ ToDoApp/ToDoApp/ToDoApp/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ToDoApp
+{
+    public class TodoItem
+    {
+        public string TodoTitle { get; set; }
+        public string TodoDes { get; set; }
+        public bool Complete { get; set; }
+        public TodoItem(string TodoTitle, string TodoDes, bool Complete)
+        {
+            this.TodoTitle = TodoTitle;
+            this.TodoDes = TodoDes;
+            this.Complete = Complete;
+        }
+    }
+}
---
+++ ToDoApp/ToDoApp/ToDoApp/TodoListViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ToDoApp
+{
+    public class TodoListViewModel
+    {
+        public ObservableCollection<TodoItem> TodoItems { get; set; }
+        public TodoListViewModel()
+        {
+            TodoItems = new ObservableCollection<TodoItem>();
+
+            TodoItems.Add(new TodoItem("Fight club", "download and watch it", false));
+            TodoItems.Add(new TodoItem("Avengers", "download and watch the movie", false));
+            TodoItems.Add(new TodoItem("Euphoria", "Download and watch the series", false));
+        }
+        public ICommand AddTodoCommand => new Command(AddTodoItem);
+        public string NewTodoTitleValue { get; set; }
+        public string NewTodoDesValue { get; set; }
+        void AddTodoItem()
+        {
+            TodoItems.Add(new TodoItem(NewTodoTitleValue, NewTodoDesValue, false));
+        }
+
+        public ICommand RemoveTodoCommand => new Command(RemoveTodoItem);
+        public string RemoveTodoTitleValue { get; set; }
+        void RemoveTodoItem(object o)
+        {
+            TodoItem todoItemBeingRemoved = o as TodoItem;
+            TodoItems.Remove(todoItemBeingRemoved);
+        }
+
+        public ICommand EditTodoCommand => new Command(EditTodoItem);
+        public String EditTodoTitleValue { get; set; }
+        void EditTodoItem(object o)
+        {
+            TodoItem todoItemBeingEdited = o as TodoItem;
+            string title = o.ToString();
+            Console.WriteLine(title);
+        }
+    }
+}
